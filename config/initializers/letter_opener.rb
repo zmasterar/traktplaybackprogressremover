c2f1191282fdeefd11174d4981a78ca8b51d495c
Benjamin Lavena <benja@zmaster.com.ar>
2020-12-17 23:42:30 -0300

    letter opener config file

--- config/initializers/letter_opener.rb
+++ config/initializers/letter_opener.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-LetterOpener.configure do |config|
-  # To overrider the location for message storage.
-  # Default value is `tmp/letter_opener`
-  config.location = Rails.root.join('tmp', 'my_mails')
+if Rails.env.development?
+  LetterOpener.configure do |config|
+    # To overrider the location for message storage.
+    # Default value is `tmp/letter_opener`
+    config.location = Rails.root.join('tmp', 'my_mails')
 
-  # To render only the message body, without any metadata or extra containers or styling.
-  # Default value is `:default` that renders styled message with showing useful metadata.
-  config.message_template = :light
+    # To render only the message body, without any metadata or extra containers or styling.
+    # Default value is `:default` that renders styled message with showing useful metadata.
+    config.message_template = :light
+  end
 end
